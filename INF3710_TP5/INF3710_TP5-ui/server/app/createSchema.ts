13a8d75a82fb5bcdd5185c38f0eb5f1842590f94
m <myriamkiriakos342@gmail.com>
2019-04-04 10:57:12 -0400

    MK FUCK GIT CAUSE CONFLIT BCP BCP

--- INF3710_TP5/INF3710_TP5-ui/server/app/createSchema.ts
+++ INF3710_TP5/INF3710_TP5-ui/server/app/createSchema.ts
@@ -1,48 +1,116 @@
 export const schema: string = `
-SET search_path = hotelDB;
+CREATE DATABASE IF NOT EXISTS VETDB;
 
-DROP SCHEMA IF EXISTS HOTELDB CASCADE;
-CREATE SCHEMA HOTELDB;
+SET search_path = VETDB;
 
-CREATE TABLE IF NOT EXISTS  HOTELDB.Hotel (
-		hotelNo		VARCHAR(10)		NOT NULL,
-		hotelName 	VARCHAR(20)		NOT NULL,
-		city		VARCHAR(50)		NOT NULL,
-		PRIMARY KEY (hotelNo));
+DROP SCHEMA IF EXISTS VETDB CASCADE;
+CREATE SCHEMA VETDB;
 
-CREATE TABLE IF NOT EXISTS HOTELDB.Room(
-roomNo VARCHAR(10) NOT NULL,
-hotelNo VARCHAR(10)	NOT NULL,
-typeroom VARCHAR(10)	NOT NULL,
-price NUMERIC(6,3) NOT NULL,
-PRIMARY KEY (roomNo, hotelNo),
-FOREIGN KEY(hotelNo) REFERENCES HOTELDB.Hotel(hotelNo) ON DELETE RESTRICT ON UPDATE CASCADE);
+DROP DOMAIN IF EXISTS VETDB.SexType;
+DROP TABLE IF EXISTS VETDB.Clinique;
+DROP TABLE IF EXISTS VETDB.Personnel;
+DROP TABLE IF EXISTS VETDB.Examen;
+DROP TABLE IF EXISTS VETDB.Traitement;
+DROP TABLE IF EXISTS VETDB.Animal;
+DROP TABLE IF EXISTS VETDB.Proprietaire;
+DROP TABLE IF EXISTS VETDB.Resultat;
 
+CREATE DOMAIN VETDB.SexType AS CHAR
+	CHECK(VALUE IN ('M','F'));
 
-CREATE DOMAIN HOTELDB.sexType AS CHAR
-	CHECK (VALUE IN ('M', 'F'));
+CREATE TABLE IF NOT EXISTS VETDB.Clinique(
+	numero VARCHAR(10) NOT NULL,
+	rue VARCHAR(20) NOT NULL,
+	ville VARCHAR(20) NOT NULL,
+	province VARCHAR(20) NOT NULL,
+	codePostal VARCHAR(6) NOT NULL,
+	gestionnaireNumero VARCHAR(10) NOT NULL,
+	telecopieur VARCHAR(12) NOT NULL,
+	telephone VARCHAR(12) NOT NULL,
+	PRIMARY KEY(numero)
+);
 
-CREATE TABLE IF NOT EXISTS HOTELDB.Guest(
-guestNo		VARCHAR(10)		NOT NULL,
-nas		VARCHAR(10)		UNIQUE NOT NULL,
-guestName 	VARCHAR(20)		NOT NULL,
-gender		sexType			DEFAULT 'M',
-guestCity	VARCHAR(50)		NOT NULL,
-PRIMARY KEY (guestNo));
+CREATE TABLE IF NOT EXISTS VETDB.Proprietaire(
+	numero VARCHAR(10) NOT NULL,
+	cliniqueNumero VARCHAR(10) NOT NULL,
+	nom VARCHAR(20) NOT NULL,
+	adresse VARCHAR(50) NOT NULL,
+	telephone VARCHAR(10) NOT NULL,
+	PRIMARY KEY(numero),
+	FOREIGN KEY(cliniqueNumero) REFERENCES VETDB.Clinique(numero)
+);
 
-CREATE TABLE IF NOT EXISTS HOTELDB.Booking(
-		hotelNo		VARCHAR(10)		NOT NULL,
-		guestNo	  	VARCHAR(10)		NOT NULL,
-		dateFrom 	DATE			NOT NULL,
-		dateTo		DATE			NULL,
-		roomNo		VARCHAR(10)		NOT NULL,
-		PRIMARY KEY (hotelNo, guestNo, roomNO, dateFrom),
-		FOREIGN KEY (guestNo) REFERENCES HOTELDB.Guest(guestNo)
-		ON DELETE SET NULL ON UPDATE CASCADE,
-		FOREIGN KEY (hotelNo, roomNo) REFERENCES HOTELDB.Room (hotelNo, roomNo)
-		ON DELETE NO ACTION ON UPDATE CASCADE,
-		CONSTRAINT date CHECK (dateTo >= dateFrom),
-		CONSTRAINT dateFrom CHECK (dateFrom >= current_date));
+CREATE TABLE IF NOT EXISTS VETDB.Employe(
+	numero VARCHAR(10) NOT NULL,
+	cliniqueNumero VARCHAR(10) NOT NULL,
+	nom VARCHAR(20) NOT NULL,
+	prenom VARCHAR(20) NOT NULL,
+	adresse VARCHAR(50) NOT NULL,
+	telephone VARCHAR(10) NOT NULL,
+	dateNaissance date NOT NULL,
+	sexe SexType DEFAULT 'M',
+	nas VARCHAR(11) UNIQUE NOT NULL,
+	fonction VARCHAR(15) NOT NULL,
+	salaire NUMERIC(10,0),
+	PRIMARY KEY(numero)
+);
 
-ALTER TABLE HOTELDB.Guest ALTER gender DROP DEFAULT;
+CREATE TABLE IF NOT EXISTS VETDB.Veterinaire(
+	numeroEmploye VARCHAR(10) NOT NULL UNIQUE,
+	estEnService bool NOT NULL,
+    PRIMARY KEY(numeroEmploye),
+	FOREIGN KEY(numeroEmploye) REFERENCES VETDB.Employe(numero)
+);
+
+CREATE TABLE IF NOT EXISTS VETDB.Traitement(
+	numero VARCHAR(10) NOT NULL,
+	description VARCHAR(50) NOT NULL,
+	cout NUMERIC(3,2) NOT NULL,
+	PRIMARY KEY(numero)
+);
+
+CREATE TABLE IF NOT EXISTS VETDB.Animal(
+	numero VARCHAR(10) NOT NULL UNIQUE,
+	proprietaireNumero VARCHAR(10) NOT NULL,
+	cliniqueNumero VARCHAR(10) NOT NULL,
+	nom VARCHAR(20) NOT NULL,
+	type VARCHAR(15) NOT NULL,
+	description VARCHAR(50) NOT NULL,
+	dateNaissance DATE NOT NULL,
+	dateInscription DATE NOT NULL,
+	etatActuel VARCHAR(15) NOT NULL,
+	PRIMARY KEY(numero, proprietaireNumero),
+	FOREIGN KEY(proprietaireNumero, cliniqueNumero) REFERENCES VETDB.Proprietaire(numero, cliniqueNumero)
+);
+
+CREATE TABLE IF NOT EXISTS VETDB.Examen(
+	numero VARCHAR(10) NOT NULL,
+	veterinaireNumero VARCHAR(10) NOT NULL,
+	numeroAnimal VARCHAR(10) NOT NULL,
+	date DATE NOT NULL,
+	heure time NOT NULL,
+	description VARCHAR(50) NOT NULL,
+	PRIMARY KEY(numero),
+	FOREIGN KEY(veterinaireNumero) REFERENCES VETDB.Veterinaire(numeroEmploye), 
+	FOREIGN KEY(numeroAnimal) REFERENCES VETDB.Animal(numero)
+);
+
+CREATE TABLE IF NOT EXISTS VETDB.Prescription(
+	numeroExamen VARCHAR(10) NOT NULL,
+	numeroTraitement VARCHAR(10) NOT NULL,
+	numeroAnimal VARCHAR(10) NOT NULL,
+	qteTraitement NUMERIC(2,0) NOT NULL,
+	dateDebut DATE NOT NULL,
+	dateFin DATE NOT NULL,
+	PRIMARY KEY(numeroExamen, numeroTraitement),
+	FOREIGN KEY(numeroExamen, numeroAnimal) REFERENCES VETDB.Examen(numero, numeroAnimal),
+	FOREIGN KEY(numeroTraitement) REFERENCES VETDB.Traitement(numero)
+);
+
+ALTER TABLE Clinique ADD CONSTRAINT cliniqueFK 
+	FOREIGN KEY (gestionnaireNumero) REFERENCES Employe(numero);
+
+ALTER TABLE Employe ADD CONSTRAINT employeFK 
+	FOREIGN KEY (cliniqueNumero) REFERENCES Clinique(numero);
+
 `;
--- INF3710_TP5/INF3710_TP5-ui/server/app/populateDB.ts
+++ INF3710_TP5/INF3710_TP5-ui/server/app/populateDB.ts
@@ -1,28 +1,100 @@
 export const data: string = `SET search_path = hotelDB;
 
-INSERT INTO HOTELDB.Hotel VALUES ('H111', 'Grosvenor Hotel', 'London');
-INSERT INTO HOTELDB.Hotel VALUES ('H112', 'Kingston Hotel', 'Kingston');
-INSERT INTO HOTELDB.Hotel VALUES ('H113', 'Hotel des pas perdus', 'Montreal');
+--Clinique
 
-INSERT INTO HOTELDB.Room VALUES ('1', 'H111', 'S', 72.00);
-INSERT INTO HOTELDB.Room VALUES ('2', 'H111', 'S', 100.00);
-INSERT INTO HOTELDB.Room VALUES ('3', 'H111', 'D', 200.00);
-INSERT INTO HOTELDB.Room VALUES ('4', 'H111', 'D', 250.00);
-INSERT INTO HOTELDB.Room VALUES ('1', 'H112', 'D', 450.00);
-INSERT INTO HOTELDB.Room VALUES ('2', 'H112', 'D', 450.00);
-INSERT INTO HOTELDB.Room VALUES ('3', 'H112', 'D', 450.00);
+INSERT INTO VETDB.Clinique(numero, rue, ville, province, codePostal, gestionnaireNumero, telecopieur, telephone)
+VALUES ('C476', '6188 rue McDonald', 'Vancouver', 'Colombie-Britanique', 'h4o0d2', 'G6523', '315-543-6421', '234-786-7654');
 
-INSERT INTO HOTELDB.Guest (guestNo, nas, guestName, gender, guestCity) VALUES ('G111', '123', 'John Smith', 'M', 'London');
-INSERT INTO HOTELDB.Guest (guestNo, nas, guestName, gender,  guestCity)VALUES ('G112', '213', 'Alex L', 'M', 'Kingston');
-INSERT INTO HOTELDB.Guest (guestNo, nas, guestName, gender,  guestCity)VALUES ('G113', '233', 'Idris S',  'M', 'Montreal');
-INSERT INTO HOTELDB.Guest (guestNo, nas, guestName, gender, guestCity) VALUES ('G114', '312', 'Guillaume D', 'M',  'Quebec');
-INSERT INTO HOTELDB.Guest (guestNo, nas, guestName, gender, guestCity)VALUES ('G115', '122', 'Katrine S.',  'F', 'Kingston');
-INSERT INTO HOTELDB.Guest (guestNo, nas, guestName, gender, guestCity)VALUES ('G116', '111', 'Simon D', 'M',  'Kingston');
+INSERT INTO VETDB.Clinique(numero, rue, ville, province, codePostal, gestionnaireNumero, telecopieur, telephone) 
+VALUES ('C456', '1234 rue Brave', 'Iqaluit', 'Nunavut', 'l3h8c8', 'G9864', '432-879-4378', '435-839-8279');
 
-INSERT INTO HOTELDB.Booking VALUES ('H111', 'G111', DATE'2019-04-01', DATE'2019-04-04', '1');
-INSERT INTO HOTELDB.Booking VALUES ('H111', 'G114', DATE'2019-04-01', DATE'2019-04-05', '3');
-INSERT INTO HOTELDB.Booking VALUES ('H111', 'G116', DATE'2019-04-03', DATE'2019-04-06', '4');
-INSERT INTO HOTELDB.Booking (hotelNo, guestNo, dateFrom, roomNo) VALUES ('H112', 'G115', DATE'2019-05-03',  '1');
+INSERT INTO VETDB.Clinique(numero, rue, ville, province, codePostal, gestionnaireNumero, telecopieur, telephone) 
+VALUES ('C176', '879 ave Hiver', 'Montreal', 'Quebec', 'h3i0v7', 'G6732', '879-555-3434', '121-919-3565');
 
-UPDATE HOTELDB.Guest set guestName = 'Alexandra L.' where guestNo='G112';
+--Proprietaire
+
+INSERT INTO VETDB.Proprietaire(numero, cliniqueNumero, nom, adresse, telephone)
+VALUES ('P456', 'C176', 'Liliane Smith', '123 rue Bouteille Montreal Qc', '587-341-3426');
+
+INSERT INTO VETDB.Proprietaire(numero, cliniqueNumero, nom, adresse, telephone)
+VALUES ('P564', 'C456', 'Mouna Mouna', '456 rue Chat Iqaluit NU', '329-624-9632');
+
+INSERT INTO VETDB.Proprietaire(numero, cliniqueNumero, nom, adresse, telephone)
+VALUES ('P653', 'C476', 'Emilien Anon', '789 rue Chien Vancouver CB', '986-234-8342');
+
+--Employe
+
+INSERT INTO VETDB.Employe(numero, cliniqueNumero, nom, prenom, adresse, telephone, dateNaissance, sexe, nas, fonction, salaire)
+VALUES ('E534', 'C456','Sheep', 'Shawn', '89 rue Baleine', '876-366-2323', '98-03-04', 'M', '647 436 436', 'Secretaire', '50 000');
+
+INSERT INTO VETDB.Employe(numero, cliniqueNumero, nom, prenom, adresse, telephone, dateNaissance, sexe, nas, fonction, salaire)
+VALUES ('E664', 'C456','Sheen', 'Salome', '74 rue Lassonde', '475-742-8765', '56-19-12', 'F', '577 774 574', 'Infirmiere', '70 000');
+
+INSERT INTO VETDB.Employe(numero, cliniqueNumero, nom, prenom, adresse, telephone, dateNaissance, sexe, nas, fonction, salaire)
+VALUES ('E876', 'C476','Rand', 'Ralph', '65 rue Blast', '786-765-7665', '78-10-28', 'M', '678 543 432', 'Secretaire', '40 000');
+
+INSERT INTO VETDB.Employe(numero, cliniqueNumero, nom, prenom, adresse, telephone, dateNaissance, sexe, nas, fonction, salaire)
+VALUES ('E876', 'C476','Elph', 'Elsa', '90 rue Neige', '756-465-3452', '67-06-06', 'F', '235 786 345', 'Infirmiere', '80 000');
+
+INSERT INTO VETDB.Employe(numero, cliniqueNumero, nom, prenom, adresse, telephone, dateNaissance, sexe, nas, fonction, salaire)
+VALUES ('E500', 'C176','Red', 'Alissa', '65 rue St-Urbain', '765-654-6544', '90-02-05', 'F', '765 465 222', 'Infirmiere', '100 000');
+
+--Examen
+
+INSERT INTO VETDB.Examen(numero, date, heure, description, veterinaireNumero, numeroAnimal)
+VALUES ('EX3422', '19-04-01','12:30', 'Examen apres traitement', 'V635', 'A532');
+
+INSERT INTO VETDB.Examen(numero, date, heure, description, veterinaireNumero, numeroAnimal)
+VALUES ('EX6543', '19-02-17','8:30', 'Examen general', 'V635', 'A532');
+
+INSERT INTO VETDB.Examen(numero, date, heure, description, veterinaireNumero, numeroAnimal)
+VALUES ('EX6749', '19-01-20','14:00', 'Examen general', 'V124', 'A087');
+
+INSERT INTO VETDB.Examen(numero, date, heure, description, veterinaireNumero, numeroAnimal)
+VALUES ('EX8765', '16-05-17','17:30', 'Urgence', 'V754', 'A907');
+
+--Veterinaire
+
+INSERT INTO VETDB.Examen(numeroEmploye, estEnService)
+VALUES ('V754', true);
+
+INSERT INTO VETDB.Examen(numeroEmploye, estEnService)
+VALUES ('V635', false);
+
+INSERT INTO VETDB.Examen(numeroEmploye, estEnService)
+VALUES ('V124', false);
+
+--Animal
+
+INSERT INTO VETDB.Animal(numero, proprietaireNumero, cliniqueNumero, nom, type, description, dateNaissance, dateInscription, etatActuel)
+VALUES ('A532', 'P456', ,'C176', 'Star', 'Chat','Sphinx sans condition preexistante', '17-01-07', '18-04-29', 'decede');
+
+INSERT INTO VETDB.Animal(numero, proprietaireNumero, cliniqueNumero, nom, type, description, dateNaissance, dateInscription, etatActuel)
+VALUES ('A087', 'P564', ,'C456', 'Blue', 'Chien','Labrador avec haute tension', '15-04-23', '16-05-23', 'vivant');
+
+INSERT INTO VETDB.Animal(numero, proprietaireNumero, cliniqueNumero, nom, type, description, dateNaissance, dateInscription, etatActuel)
+VALUES ('A907', 'P653', ,'C476', 'Silver', 'Serpent','probleme d'ecailles', '12-05-04', '14-07-19', 'vivant');
+
+--Traitement
+
+INSERT INTO VETDB.Traitement(numero, description, cout)
+VALUES ('T432', 'Bisoprolol 5mg tout les soirs avant de manger', '50$');
+
+INSERT INTO VETDB.Traitement(numero, description, cout)
+VALUES ('T365', 'Bandages, platre sur patte droite et advils', '200$');
+
+NSERT INTO VETDB.Traitement(numero, description, cout)
+VALUES ('T909', 'Appliquer creme hydratante trois fois par jour', '754$');
+
+--Prescription
+
+INSERT INTO VETDB.Prescription(numeroExamen, numeroTraitement, qteTraitement, dateDebut, dateFin)
+VALUES ('A087', 'T432', 30, '17-10-30', '17-11-30' );
+
+INSERT INTO VETDB.Prescription(numeroExamen, numeroTraitement, qteTraitement, dateDebut, dateFin)
+VALUES ('A532', 'T365', 15, '18-09-24', '17-10-08' );
+
+INSERT INTO VETDB.Prescription(numeroExamen, numeroTraitement, qteTraitement, dateDebut, dateFin)
+VALUES ('A907', 'T909', 21, '17-04-00', '17-04-07' );
+
 ;`;
