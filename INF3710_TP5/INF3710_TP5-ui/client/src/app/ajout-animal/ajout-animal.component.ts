7aa086cd2a19a4ff8aff83f072f1a12fa2b20119
m <myriamkiriakos342@gmail.com>
2019-04-08 13:48:48 -0400

    Pt la ca fonctionne?

--- INF3710_TP5/INF3710_TP5-ui/client/src/app/ajout-animal/ajout-animal.component.ts
+++ INF3710_TP5/INF3710_TP5-ui/client/src/app/ajout-animal/ajout-animal.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Clinique } from "../../../../common/tables/Clinique";
+import { Proprietaire } from "../../../../common/tables/Proprietaire";
+import { CommunicationService } from "../communication.service";
 
 @Component({
-  selector: 'app-ajout-animal',
-  templateUrl: './ajout-animal.component.html',
-  styleUrls: ['./ajout-animal.component.css']
+  selector: "app-ajout-animal",
+  templateUrl: "./ajout-animal.component.html",
+  styleUrls: ["./ajout-animal.component.css"]
 })
 export class AjoutAnimalComponent implements OnInit {
+  public cliniques: Clinique[];
+  public proprietaires: Proprietaire[];
 
-  constructor() { }
+  public constructor(private communicationService: CommunicationService) { 
+    this.cliniques = [];
+    this.proprietaires = [];
+  }
 
-  ngOnInit() {
+  public getClinique(): void {
+    this.communicationService.getCliniques().subscribe((cliniques: Clinique[]) =>
+    this.cliniques = cliniques);
+  }
+
+  public getProprietaire(): void {
+    this.communicationService.getProprietaires().subscribe((proprietaires: Proprietaire[]) =>
+    this.proprietaires = proprietaires);
+  }
+
+  public ngOnInit(): void {
+    this.getClinique();
+    this.getProprietaire();
   }
 
 }
